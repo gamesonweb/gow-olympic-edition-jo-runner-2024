571137272460a05b96b8f288f2bd19b3325b7f33
Romain PATUREAU <romain.patureau@outlook.com>
2024-05-21 10:59:25 +0200

    Les chunks entre les joueurs restent générés

--- src/js/engine/game/world/WorldMap.ts
+++ src/js/engine/game/world/WorldMap.ts
@@ -6,32 +6,38 @@ import RunnerEngine from "../../RunnerEngine.ts";
 export default class WorldMap{
     private chunks : WorldChunk[];
     private scene : Scene;
-    private lastChunkIndex:number = 0
+    private lastChunkIndex:number = 0;
+    private firstChunkIndex:number = 0;
+    private displayedChunks:number;
 
     private static HIDDING_OFFSET = 1.25;
     constructor(scene : Scene,displayedChunks : number = 3){
         this.scene = scene;
         this.chunks = [];
+        this.displayedChunks = displayedChunks;
         for(let i = 0 ; i<displayedChunks ; i++){
             this.createChunk(i)
             this.lastChunkIndex=i;
         }
+        this.firstChunkIndex = this.chunks[0].getIndex();
     }
 
 
     update(deltaTime : number ){
         // console.log(deltaTime)
-        this.chunks.forEach((chunk) =>{
-            // console.log(RunnerEngine.getPlayer(0).getZ())
-            if (chunk.getZ()+WorldMap.HIDDING_OFFSET<RunnerEngine.getPlayer(0).getZ()){
-                //remove passed chunk
-                this.removeChunk(chunk);
-                //create new chunk
-                this.lastChunkIndex++;
-                this.createChunk(this.lastChunkIndex);
 
-            }
-        })
+        //remove passed chunk of last place
+        let lastPlaceChunk : WorldChunk = this.chunks[this.firstChunkIndex];
+        if (lastPlaceChunk.getZ()+WorldMap.HIDDING_OFFSET < Math.min(RunnerEngine.getPlayer(0).getZ(), RunnerEngine.getPlayer(1).getZ())){
+            this.removeChunk(lastPlaceChunk);
+        }
+
+        //create new chunk for first place
+        let firstPlaceChunk : WorldChunk = this.chunks[this.lastChunkIndex - this.displayedChunks+1];
+        if (firstPlaceChunk.getZ()+WorldMap.HIDDING_OFFSET < Math.max(RunnerEngine.getPlayer(0).getZ(), RunnerEngine.getPlayer(1).getZ())){
+            this.lastChunkIndex++;
+            this.createChunk(this.lastChunkIndex);
+        }
     }
 
     removeChunk(chunk: WorldChunk){
